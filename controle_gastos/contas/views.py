8e757eccc6e821aa6f89b207286d35add2af2887
eduardo-almeida <eduardoeandroid@gmail.com>
2020-11-24 11:46:58 -0300

    Fim do Estudo

--- controle_gastos/contas/views.py
+++ controle_gastos/contas/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import datetime
 
 def home (request):
-    return render(request, 'contas/home.html')
+    data = {}
+    data['agora'] = datetime.datetime.now()
+    return render(request, 'contas/home.html', data)
