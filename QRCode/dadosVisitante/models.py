30c777d6484fa1230c6c78f1cb43c404f1e157e0
eduardo-almeida <eduardoeandroid@gmail.com>
2020-12-03 07:51:24 -0300

    Atualizacoes de script

--- QRCode/dadosVisitante/models.py
+++ QRCode/dadosVisitante/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# pylint: disable=import-error
+from cpf_field.models import CPFField
 
-# Create your models here.
+#from cep.widgets import CEPInput
+
+
+class Visitante(models.Model):
+    nome = models.CharField(max_length=150, help_text="Nome do Visitante")
+    cpf = CPFField('cpf')
+    #endereco
+    #cep
+    #telefone
+    #email
